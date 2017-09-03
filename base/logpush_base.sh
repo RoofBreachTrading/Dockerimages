90104cf7e8aeb03812119ef0e570db77df46bf83
Michael Ritter <drissg@gmail.com>
2017-09-03 15:55:55 +0200

    Adds initial version of logpush script

--- base/logpush_base.sh
+++ base/logpush_base.sh
@@ -1,13 +1,30 @@
-# if config is not set: return
+#!/bin/bash
 
-# Required config:
-# - Metric name
-# - Namespace name
-# - Login (id/secret)
-# - Region
+if [[ "$RBT_LOG_STATE" == "disabled" ]]; then
+    exit 0
+fi
+
+# if not initialized:
+if [[ "$RBT_LOG_STATE" == "" ]]; then
+    # if config is not set: return
+    if [[ "$RBT_LOG_METRIC_NAME" == "" || "$RBT_LOG_NAMESPACE"  == "" || "$RBT_LOG_REGION" == "" || "$RBT_LOG_KEY_ID" == "" || "$RBT_LOG_ACCESS_KEY" == "" ]]; then
+        RBT_LOG_STATE="disabled"
+        exit 0
+    fi
+
+    # login to aws
+    echo "[default]
+    region=$RBT_LOG_REGION
+    output=text" > ~/.aws/config
+    echo "[default]
+    aws_access_key_id=$RBG_LOG_KEY_ID
+    aws_secret_access_key=$RBT_LOG_ACCESS_KEY" > ~/.aws/credentials
+
+    RBT_LOG_STATE="enabled"
+fi
 
 # If config exists:
- # Setup the command used for pushing the logs
- # aws cloudwatch put-metric-data --metric-name PageViewCount --namespace MyService --value 2 --timestamp 2016-10-14T12:00:00.000Z
  # Get the logs
+ VALUE=$(logpush.sh)
  # Push the logs
+ aws cloudwatch put-metric-data --metric-name "$RBT_LOG_METRIC_NAME" --namespace "$RBT_LOG_NAMESPACE" --value "$VALUE"
